099a036469731c2b32484ab4efaa4605b5851183
Dzmitry Dubovik <dim3dubovik7@gmail.com>
2024-07-07 01:28:42 +0200

    update url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://wvolf3g561.execute-api.eu-west-1.amazonaws.com/prod",
+  product: "https://p0w5kh033b.execute-api.eu-west-1.amazonaws.com/prod/",
   order: "https://wvolf3g561.execute-api.eu-west-1.amazonaws.com/prod",
   import: "https://1r6v7l9rog.execute-api.eu-west-1.amazonaws.com/prod",
-  bff: "https://wvolf3g561.execute-api.eu-west-1.amazonaws.com/prod",
-  cart: "https://wvolf3g561.execute-api.eu-west-1.amazonaws.com/prod",
+  bff: "https://p0w5kh033b.execute-api.eu-west-1.amazonaws.com/prod/",
+  cart: "https://p0w5kh033b.execute-api.eu-west-1.amazonaws.com/prod/",
 };
 
 export default API_PATHS;
